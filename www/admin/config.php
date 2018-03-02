06c7a2bb53b1758075af9ef56849fc17ea41ca3f
Dave-Slave <36995431+Dave-Slave@users.noreply.github.com>
2018-03-02 18:36:48 +0200

    Initial commit

---
+++ www/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://intech-group/admin/');
+define('HTTP_CATALOG', 'http://intech-group/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://intech-group/admin/');
+define('HTTPS_CATALOG', 'http://intech-group/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\intech-group\www/admin/');
+define('DIR_SYSTEM', 'Z:\home\intech-group\www/system/');
+define('DIR_LANGUAGE', 'Z:\home\intech-group\www/admin/language/');
+define('DIR_TEMPLATE', 'Z:\home\intech-group\www/admin/view/template/');
+define('DIR_CONFIG', 'Z:\home\intech-group\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\intech-group\www/image/');
+define('DIR_CACHE', 'Z:\home\intech-group\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\intech-group\www/system/download/');
+define('DIR_UPLOAD', 'Z:\home\intech-group\www/system/upload/');
+define('DIR_LOGS', 'Z:\home\intech-group\www/system/logs/');
+define('DIR_MODIFICATION', 'Z:\home\intech-group\www/system/modification/');
+define('DIR_CATALOG', 'Z:\home\intech-group\www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'intech');
+define('DB_PASSWORD', 'intech');
+define('DB_DATABASE', 'intech');
+define('DB_PREFIX', 'oc_');
---
+++ www/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://intech-group/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://intech-group/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\intech-group\www/catalog/');
+define('DIR_SYSTEM', 'Z:\home\intech-group\www/system/');
+define('DIR_LANGUAGE', 'Z:\home\intech-group\www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:\home\intech-group\www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:\home\intech-group\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\intech-group\www/image/');
+define('DIR_CACHE', 'Z:\home\intech-group\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\intech-group\www/system/download/');
+define('DIR_UPLOAD', 'Z:\home\intech-group\www/system/upload/');
+define('DIR_MODIFICATION', 'Z:\home\intech-group\www/system/modification/');
+define('DIR_LOGS', 'Z:\home\intech-group\www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'intech');
+define('DB_PASSWORD', 'intech');
+define('DB_DATABASE', 'intech');
+define('DB_PREFIX', 'oc_');
